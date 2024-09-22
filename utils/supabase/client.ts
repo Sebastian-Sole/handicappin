6bcc19c2e49a3f9bd45b548f70484bd029887781
Sebastian Sole <sebastrs@stud.ntnu.no>
2024-09-21 18:50:10 -1000

    clean: remove unknown code

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -4,11 +4,6 @@ import { createBrowserClient } from "@supabase/ssr";
 export function createClientComponentClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookieOptions: {
-        secure: process.env.NODE_ENV === "production",
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
