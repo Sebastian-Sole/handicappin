255b14aca998a6c360142918e3bb3917c1059f30
Sebastian Sole <71325332+Sebastian-Sole@users.noreply.github.com>
2025-07-26 03:38:02 +0200

    ⚰️ clean: replace svg code with lucide icons (#45)
    Replaced svg code with lucide icons and removed unused svg files

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -15,6 +15,7 @@ import {
 import {
   CirclePlus,
   LayoutDashboardIcon,
+  Menu,
   SettingsIcon,
   UserIcon,
   UserRound,
@@ -130,7 +131,7 @@ export async function Navbar() {
                       size="icon"
                       className="rounded-full"
                     >
-                      <MenuIcon className="h-6 w-6" />
+                      <Menu className="h-6 w-6" />
                     </Button>
                   </SheetTrigger>
                   <SheetContent side="right" className="xs:w-64">
@@ -221,7 +222,7 @@ export async function Navbar() {
                       size="icon"
                       className="rounded-full"
                     >
-                      <MenuIcon className="h-6 w-6" />
+                      <Menu className="h-6 w-6" />
                     </Button>
                   </SheetTrigger>
                   <SheetContent side="right" className="xs:w-64">
@@ -272,53 +273,5 @@ export async function Navbar() {
         )}
       </div>
     </header>
-  );
-}
-
-function MenuIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
-  );
-}
-
-function SunIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="4" />
-      <path d="M12 2v2" />
-      <path d="M12 20v2" />
-      <path d="m4.93 4.93 1.41 1.41" />
-      <path d="m17.66 17.66 1.41 1.41" />
-      <path d="M2 12h2" />
-      <path d="M20 12h2" />
-      <path d="m6.34 17.66-1.41 1.41" />
-      <path d="m19.07 4.93-1.41 1.41" />
-    </svg>
   );
 }
