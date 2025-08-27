ec8a0bcda12c1381fc1e45de66683b072ab029a8
Sebastian Sole <sebastrs@stud.ntnu.no>
2025-08-27 20:55:18 +0200

    ✨ feat: landing page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { HomePage } from "@/components/homepage/home-page";
 import { api } from "@/trpc/server";
 import { createServerComponentClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import HomepageSkeleton from "@/components/loading/homepage-skeleton";
+import Landing from "@/components/homepage/landing";
 
 export default async function Home() {
   const supabase = await createServerComponentClient();
@@ -12,7 +12,7 @@ export default async function Home() {
   const userId = data.user?.id;
 
   if (!userId) {
-    redirect("/login");
+    return <Landing />;
   }
 
   const profile = await api.auth.getProfileFromUserId(userId);
